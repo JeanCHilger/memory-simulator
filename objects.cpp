ae22145714ef72d0ccbb197114dbc1240b3c9e20
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2019-12-06 20:42:12 -0300

    inserts cache basic setup

--- objects.cpp
+++ objects.cpp
@@ -17,7 +17,7 @@ struct block {
      * Address = block + values[i].offset.
      * */
 
-    char number[5];
+    char blockNumber[5];
 
     Cell values[4];
 };
@@ -25,7 +25,12 @@ struct block {
 typedef struct block Block;
 
 struct row {
-    char number[3];
+    /*
+     * Address = tag + number + values[i].offset.
+     * */
+
+    char tag[2];
+    char rowNumber[3];
 
     Cell values[4];
 };
@@ -48,7 +53,7 @@ class PrimaryMemory {
              * */
 
             for (int address=0; address < this -> CELLS; address++) {
-                this -> insertData(address, 120);
+                this -> insertData(address, rand() % 127);
             }
 
         }
@@ -82,7 +87,10 @@ class PrimaryMemory {
             int blockNumber = address % this -> BLOCKS;
             int offset = address % this -> BLOCK_SIZE;
 
-            strncpy(this -> memory[blockNumber].values[offset].value, "0000000", sizeof(this -> memory[blockNumber].values[offset].value) - 1);
+            strncpy(this -> memory[blockNumber].values[offset].value, 
+                    "0000000", 
+                    sizeof(this -> memory[blockNumber].values[offset].value) - 1);
+
             this -> memory[blockNumber].values[offset].value[sizeof(this -> memory[blockNumber].values[offset].value) - 1] = 0;
         }
 
@@ -101,6 +109,50 @@ class PrimaryMemory {
 };
 
 class CacheMemory {
+    /*
+     * */
+
+    public:
+
+        CacheMemory () {
+
+        }
+
+        char * getData(int address) {
+            /*
+             * Returns the data located at the given address.
+             *
+             * Parameters: addres -> int
+             * Return    : value -> char *
+             * */
+
+            int blockNumber = address % this -> PM_BLOCKS;
+            int rowNumber = blockNumber % this -> ROWS;
+            int offset = address % this -> ROW_SIZE;
+
+            //Block block = this -> memory[blockNumber];
+            //Cell cell = block.values[offset];
+
+            Row row = this -> memory[rowNumber];
+            int tag = blockNumber - rowNumber;
+
+            // hit
+            if (row.tag == "10"/*decimalToBinary(tag)*/) {
+                return this -> memory[rowNumber].values[offset];
+            // miss
+            } else {
+
+            }
+            
+         }
+
+
+    private:
+        Row memory[8];
+        const int ROWS = 8;
+        const int ROW_SIZE = 4;
+        const int CELLS = 32;
+        const int PM_BLOCKS = 32;
 
 };
 
